803d0dc4d6f741fdba3d1af4796ef4bef3cea8e0
gintariukai <gintariukai@hotmail.com>
2023-02-12 19:28:11 +0200

    Add style to index.css Header files, create menu, add new images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ export default function Header() {
     return (
         <header>
             <div>
-                <span className='logo'>House Staff</span>
+                <span className='logo'>Nomeda Hair Boutique</span>
+                <ul className='nav'>
+                    <li>Shop</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </div>
             <div className='presentation'></div>
         </header>
