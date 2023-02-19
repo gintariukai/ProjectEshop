595d4731082565c0a21bfcc14df5791b139f1622
gintariukai <gintariukai@hotmail.com>
2023-02-19 18:02:24 +0200

    Added npm react-icon, add style to icon-bottom, add array let with function setCartOpen, add block cartOpen with class shop-cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
+import {FaShoppingCart} from "react-icons/fa";
 
 export default function Header() {
+    let [cartOpen, setCartOpen] = useState(false)
+
     return (
         <header>
             <div>
@@ -10,6 +13,11 @@ export default function Header() {
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
+                <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && "active"}`} />
+
+                {cartOpen && (
+                    <div className="shop-cart"></div>
+                )}
             </div>
             <div className="presentation"></div>
         </header>
